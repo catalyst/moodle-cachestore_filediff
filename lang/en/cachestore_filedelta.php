8b262d3d7a61b2d5fc6219abe1bb8c71b47b994f
Brendan Heywood <brendan@catalyst-au.net>
2026-02-22 18:08:02 +1100

    Fix lang

--- lang/en/cachestore_filedelta.php
+++
@@ -1,5 +0,0 @@
-<?php
-$string['pluginname'] = 'File deltacache store';
-$string['filedelta:path'] = 'Storage path';
-$string['filedelta:missinterval'] = 'Forced miss interval';
-
---
+++ lang/en/cachestore_filediff.php
@@ -0,0 +1,4 @@
+<?php
+$string['pluginname'] = 'File delta cache store';
+$string['filediff:path'] = 'Storage path';
+
