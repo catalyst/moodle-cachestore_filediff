a0082b50bcbf1469ca92ef9f7917767052026d26
Brendan Heywood <brendan@catalyst-au.net>
2026-02-22 16:02:58 +1100

    Add reset cli

---
+++ cli/reset.php
@@ -0,0 +1,11 @@
+<?php
+define('CLI_SCRIPT', true);
+define('IGNORE_COMPONENT_CACHE', true);
+
+require(__DIR__.'/../../../../config.php');
+require_once($CFG->libdir.'/clilib.php');
+
+cli_heading("Reset all snapshots");
+fulldelete("$CFG->dataroot/deltagibbon/");
+
+unset_config('snapshot', 'cachestore_deltagibbon');
